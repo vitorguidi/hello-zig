d1e718a36f1035722b9ed1a591535a8ca626d0d4
Vitor Guidi <vitorguidi@gmail.com>
2026-06-01 18:45:30 -0700

    Make linked list polymorphic with structs and comptime

--- src/linked_list.zig
+++ src/linked_list.zig
@@ -2,41 +2,60 @@
 const std = @import("std");
 const Io = std.Io;
 
-const Node = struct {
-    val: i32,
-    next: ?*Node,
-};
+pub fn Node(comptime T: type) type {
+    return struct {
+        val: T,
+        next: ?*@This(),
+    };
+}
 
-const LinkedList = struct {
-    head: ?*Node,
-    allocator: std.mem.Allocator,
+pub fn LinkedList(comptime T: type) type {
+    const NodeT = Node(T);
+    return struct {
+        head: ?*NodeT,
+        allocator: std.mem.Allocator,
 
-    pub fn init(allocator: std.mem.Allocator) LinkedList {
-        return .{ .head = null, .allocator = allocator };
-    }
-
-    pub fn deinit(self: *@This()) void {
-        var cur = self.head;
-        while (cur) |n| {
-            cur = n.next;
-            self.allocator.destroy(n);
+        pub fn init(allocator: std.mem.Allocator) @This() {
+            return .{ .head = null, .allocator = allocator };
         }
-    }
 
-    pub fn push(self: *@This(), val: i32) !void {
-        const node = try self.allocator.create(Node);
-        node.* = .{ .val = val, .next = self.head };
-        self.head = node;
-    }
-};
+        pub fn deinit(self: *@This()) void {
+            var cur = self.head;
+            while (cur) |n| {
+                cur = n.next;
+                self.allocator.destroy(n);
+            }
+        }
 
-test "append" {
-    var l = LinkedList.init(std.testing.allocator);
+        pub fn push(self: *@This(), val: i32) !void {
+            const node = try self.allocator.create(NodeT);
+            node.* = .{ .val = val, .next = self.head };
+            self.head = node;
+        }
+    };
+}
+
+test "append_i32" {
+    var l = LinkedList(i32).init(std.testing.allocator);
     defer l.deinit();
 
     try l.push(1);
     try l.push(2);
     try l.push(3);
+
+    var cur = l.head;
+    while (cur) |n| : (cur = n.next) {
+        std.debug.print("val = {d}\n", .{n.val});
+    }
+}
+
+test "append_f64" {
+    var l = LinkedList(f64).init(std.testing.allocator);
+    defer l.deinit();
+
+    try l.push(1.0);
+    try l.push(2.0);
+    try l.push(3.0);
 
     var cur = l.head;
     while (cur) |n| : (cur = n.next) {
