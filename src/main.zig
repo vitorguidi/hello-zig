cdd9dfd7e6a316e1c9f53983abdc0bd780775843
Vitor Guidi <vitorguidi@gmail.com>
2026-06-01 18:22:25 -0700

    Add zig boilerplate

---
+++ src/main.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+const Io = std.Io;
+
+const hello_zig = @import("hello_zig");
+
+pub fn main(_: std.process.Init) !void {}
+
+test "simple test" {}
---
+++ src/root.zig
@@ -0,0 +1,3 @@
+//! By convention, root.zig is the root source file when making a package.
+const std = @import("std");
+const Io = std.Io;
